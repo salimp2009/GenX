680a6f526d4c339a46f4e724423def2356bd3654
Salim <salimp2009@gmail.com>
2019-05-15 01:07:55 +0300

    Added Diffuse Material Effect; - added random_in_unit_sphere function to pick a random point in a unit radius sphere  that originates from hitpoint p within a unit sphere which has a center of p+N

--- RayTracing_OneWeekend/RayTracing_OneWeekend.cpp
+++ RayTracing_OneWeekend/RayTracing_OneWeekend.cpp
@@ -13,52 +13,52 @@
 // Vec3 completed
 // Ray.hpp added;
 
-void vec3_test() 
-{
-	// test cases for Vec3
-	Vec3 points{ 1.1f,1.2f,1.3f };
-	Vec3 point2{ 3.0f, 4.0f, 5.0f };
-	std::cout << "\nPoints: ";
-	std::cout << points.x() << " " << points.y() << " " << points.z() << '\n';
-	std::cout << points.length() << '\n';
-	Vec3 point3 = -point2;
-	std::cout << point3[0] << '\n';
-	point3[2] = 10;
-	std::cout << point3[2] << '\n';
-	std::cout << point2 << '\n';
+//void vec3_test() 
+//{
+//	// test cases for Vec3
+//	Vec3 points{ 1.1f,1.2f,1.3f };
+//	Vec3 point2{ 3.0f, 4.0f, 5.0f };
+//	std::cout << "\nPoints: ";
+//	std::cout << points.x() << " " << points.y() << " " << points.z() << '\n';
+//	std::cout << points.length() << '\n';
+//	Vec3 point3 = -point2;
+//	std::cout << point3[0] << '\n';
+//	point3[2] = 10;
+//	std::cout << point3[2] << '\n';
+//	std::cout << point2 << '\n';
+//
+//	point3.make_unit_vector();
+//	std::cout << point3 << '\n';
+//}
 
-	point3.make_unit_vector();
-	std::cout << point3 << '\n';
-}
-
-void ppm_test()
-{
-	std::string file{ "ppmexample.ppm" };
-	std::ofstream ost(file);
-
-	int nx{ 200 };
-	int ny{ 100 };
-
-	ost << "P3\n" << nx << " " << ny << "\n255\n";
-	for (int j = ny - 1; j >= 0; --j)
-	{
-		for (int i = 0; i < nx; ++i)
-		{
-			float r = float(i) / float(nx);
-			float g = float(j) / float(ny);
-			float b = 0.2f;
-			Vec3 col{ float(i) / float(nx), float(j) / float(ny), 0.2f };
-
-			int ir = int(255.99 * col[0]);
-			int ig = int(255.99 * col[1]);
-			int ib = int(255.99 * col[2]);
-
-		//	std::cout << ir << " " << ig << " " << ib << '\n';
-			ost << ir << " " << ig << " " << ib << "\n";
-		}
-	}
-	ost.close();
-}
+//void ppm_test()
+//{
+//	std::string file{ "ppmexample.ppm" };
+//	std::ofstream ost(file);
+//
+//	int nx{ 200 };
+//	int ny{ 100 };
+//
+//	ost << "P3\n" << nx << " " << ny << "\n255\n";
+//	for (int j = ny - 1; j >= 0; --j)
+//	{
+//		for (int i = 0; i < nx; ++i)
+//		{
+//			float r = float(i) / float(nx);
+//			float g = float(j) / float(ny);
+//			float b = 0.2f;
+//			Vec3 col{ float(i) / float(nx), float(j) / float(ny), 0.2f };
+//
+//			int ir = int(255.99 * col[0]);
+//			int ig = int(255.99 * col[1]);
+//			int ib = int(255.99 * col[2]);
+//
+//		//	std::cout << ir << " " << ig << " " << ib << '\n';
+//			ost << ir << " " << ig << " " << ib << "\n";
+//		}
+//	}
+//	ost.close();
+//}
 
 // Sphere Equation; sphere center is at (cx,cy,cz)
 // Equation to check if(x,y,z) is on the sphere;
@@ -69,37 +69,55 @@ void ppm_test()
 // Solving dot((p-c), (p-c))=R*R; equation to check if the ray hits the sphere in vector form
 // p(t)= A+t*B; ray equation
 // Solving the equation for unknown t; t*t*dot(B,B)+2*t*dot(B,A-C)+dot(A-C,A-C)-R*R =0
-float hit_sphere(const Vec3& center, float radius, const Ray& r) 
+
+//float hit_sphere(const Vec3& center, float radius, const Ray& r) 
+//{
+//	Vec3 oc = r.origin() - center;							// A-C; A is the origin of the ray, C is the center of sphere
+//	float a = dot(r.direction(), r.direction());			// dot(B, B); B=r.direction()
+//	float b = 2 * dot(oc, r.direction());					// dot(B, A-C) in original formula but here it is dot(A-C, B) which are equal
+//	float c = dot(oc, oc)-radius*radius;					// dot(A-C, A-C)-R*R; R is the radius
+//	float discriminant = b * b - 4 * a * c;					// if the above hit_sphere equation will be solved for t; // discriminant has to be positive to have real roots
+//	if (discriminant < 0) {		
+//		return -1.0f;										// condition if no real roots; the color of the sphere will be blue-white gradient
+//	}
+//	else 
+//	{
+//		return (-b-sqrt(discriminant)) /(2.0f*a);			// tmin=-b-sqrt(b*b-4*a*c) / 2*a
+//	}
+//								
+//}
+
+Vec3 random_in_unit_sphere()
 {
-	Vec3 oc = r.origin() - center;							// A-C; A is the origin of the ray, C is the center of sphere
-	float a = dot(r.direction(), r.direction());			// dot(B, B); B=r.direction()
-	float b = 2 * dot(oc, r.direction());					// dot(B, A-C) in original formula but here it is dot(A-C, B) which are equal
-	float c = dot(oc, oc)-radius*radius;					// dot(A-C, A-C)-R*R; R is the radius
-	float discriminant = b * b - 4 * a * c;					// if the above hit_sphere equation will be solved for t; // discriminant has to be positive to have real roots
-	if (discriminant < 0) {		
-		return -1.0f;										// condition if no real roots; the color of the sphere will be blue-white gradient
-	}
-	else 
-	{
-		return (-b-sqrt(discriminant)) /(2.0f*a);			// tmin=-b-sqrt(b*b-4*a*c) / 2*a
-	}
-								
+	std::random_device rd;									// will be used to obtain a seed for random number engine
+	std::mt19937 gen(rd());									// mersenne_twister_engine seeded with rd()	; 				
+	std::uniform_real_distribution<>dis(0.0f, 1.0f);
+	Vec3 p;
+	do {
+		p = 2.0f * Vec3(dis(gen), dis(gen), dis(gen)) - Vec3(1.0f, 1.0f, 1.0f);
+	} while (p.squared_length() >= 1.0f);
+	return p;
 }
+
 
 // Adding Surface Normal to color function; 
 // Normal is calculated as unit vector in the direction of the hitpoint minus the center; P-C 
-
 Vec3 color(const Ray& r, Hitable *world)
 {
 	hit_record rec;
-	if(world->hit(r, 0.0f, FLT_MAX, rec)) 
+	if(world->hit(r, 0.001f, FLT_MAX, rec)) 
 	{
-		return 0.5f* Vec3(rec.normal.x() + 1.0f, rec.normal.y()+1.0f, rec.normal.z()+1.0f);
+		Vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+		return 0.5f*color(Ray(rec.p, target-rec.p), world);
+	}
+	else
+	{
+		Vec3 unit_direction = unit_vector(r.direction());
+		float t = 0.5f * (unit_direction.y() + 1.0f);
+		return (1.0f - t) * Vec3(1.0f, 1.0f, 1.0f) + t * Vec3(0.5f, 0.7f, 1.0f);
 	}
 																						   
-	Vec3 unit_direction = unit_vector(r.direction());											
-	float t = 0.5f * (unit_direction.y() + 1.0f);
-	return (1.0f - t) * Vec3(1.0f, 1.0f, 1.0f) + t * Vec3(0.5f, 0.7f, 1.0f);
+	
 }
 
 int main()
@@ -143,7 +161,7 @@ int main()
 				col += color(r, world);							// get the color if there is any hit
 			}
 			col  /= float(ns);									// use tge average of sample colors for a more smooth edge coloring; antialiasing
-
+			col = Vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
 			int ir = int(255.99 * col[0]);
 			int ig = int(255.99 * col[1]);
 			int ib = int(255.99 * col[2]);
